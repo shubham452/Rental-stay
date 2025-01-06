a34c9af61991cade9035d7ed28377eec6e9a09f8
shubham452 <shubham.sk70@gmail.com>
2025-01-06 21:37:45 +0530

    connected to database

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+const app=express();
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("connected to mongo db")
+}).catch((err)=>{
+    console.log(err);
+})
+
+app.use(express.json())
+app.use(cors());
+app.use(express.static("public"))
+
+app.listen(3000, ()=>{
+    console.log("server is running in port 3000")
+})
