69508cd855259763e12119b8e94d1ed9f61a8d7c
shubham452 <shubham.sk70@gmail.com>
2025-01-07 08:58:56 +0530

    auth router

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import multer from 'multer';
+
+const router = express.Router()
+
+
+const storage= multer.diskStorage({
+    destination:function(req,file,cb)
+    {
+        cb(null,"public/uploads/")
+    },
+
+filename:function(req,file,cb)
+{
+    cb(null,file.originalname)
+},
+})
+
+const upload = multer({storage})
+
+router.post("/register",upload.single("profileImage"),register)
+router.post("/login",login)
+
+
+export default router
